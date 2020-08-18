9bc923e9fff97c9948c31896fdf4482b2ce87dd7
Justin Mattson <justin.mattson@outlook.com>
2020-08-17 18:58:44 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Todos from "./components/todos/Todos";
+import AddTodo from "./components/todos/AddTodo";
+import About from "./components/pages/About";
+import axios from "axios";
+// import uuid from "uuid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  state = {
+    todos: [
+      // Attempted import error: 'uuid' does not contain a default export (imported as 'uuid').
+      // {
+      //   id: uuid.v4(),
+      //   title: "",
+      //   completed: false,
+      // },
+    ],
+  };
+
+  componentDidMount() {
+    axios
+      .get("http://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then((res) => this.setState({ todos: res.data }));
+  }
+  // Toggle Completed
+  toggleComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    });
+  };
+
+  // Delete Todo
+  delTodo = (id) => {
+    axios
+      .delete(`http://jsonplaceholder.typicode.com/todos/${id}`)
+      .then((res) =>
+        this.setState({
+          todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+        })
+      );
+  };
+
+  // Add Todo
+  addTodo = (title) => {
+    axios
+      .post("http://jsonplaceholder.typicode.com/todos", {
+        title,
+        completed: false,
+      })
+      .then((res) => this.setState({ todos: [...this.state.todos, res.data] }));
+    // Really should put a catch statement in here.
+  };
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    toggleComplete={this.toggleComplete}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>
+        This is the{" "}
+        <a href="https://www.youtube.com/watch?v=sBws8MSXN7A">
+          TodoList app v1.0.0.
+        </a>{" "}
+        It is part of a React crash course.
+      </p>
+    </React.Fragment>
+  );
+}
+
+export default About;
