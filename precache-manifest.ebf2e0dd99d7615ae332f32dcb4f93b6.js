12258cffe75e3a373f883c74adf13b7a5fae5cfa
Justin Mattson <justin.mattson@outlook.com>
2020-08-17 19:02:14 -0600

    Updates

---
+++ precache-manifest.ebf2e0dd99d7615ae332f32dcb4f93b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ddec3206df1f439e58ea7a121a2b1e",
+    "url": "/todos-react/index.html"
+  },
+  {
+    "revision": "e7a9a646dc19e365caf2",
+    "url": "/todos-react/static/css/main.1e4fe28f.chunk.css"
+  },
+  {
+    "revision": "91d1adab4a6f8ed5c49b",
+    "url": "/todos-react/static/js/2.afb096a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todos-react/static/js/2.afb096a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a9a646dc19e365caf2",
+    "url": "/todos-react/static/js/main.0bcc51f1.chunk.js"
+  },
+  {
+    "revision": "2b607e1ae607ce9060ea",
+    "url": "/todos-react/static/js/runtime-main.30f2425a.js"
+  }
+]);
